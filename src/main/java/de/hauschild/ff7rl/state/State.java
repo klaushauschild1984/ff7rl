7e4d9b031098cfa808524063b5d33b02c98e1a1b
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2015-07-24 00:01:01 +0200

    update copyright

--- src/main/java/de/hauschild/ff7rl/state/State.java
+++ src/main/java/de/hauschild/ff7rl/state/State.java
@@ -1,7 +1,8 @@
-/* Copyright (C) Klaus Hauschild - All Rights Reserved
+/*
+ * Copyright (C) Klaus Hauschild - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Klaus Hauschild <klaus.hauschild.1984@gmail,com>, 2014
+ * Written by Klaus Hauschild <klaus.hauschild.1984@gmail,com>, 2015
  */
 package de.hauschild.ff7rl.state;
 
--- src/test/java/de/hauschild/ff7rl/ui/ScreenMenuTest.java
+++ src/test/java/de/hauschild/ff7rl/ui/ScreenMenuTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Klaus Hauschild - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Klaus Hauschild <klaus.hauschild.1984@gmail,com>, 2015
+ */
 package de.hauschild.ff7rl.ui;
 
 import org.testng.annotations.Test;
