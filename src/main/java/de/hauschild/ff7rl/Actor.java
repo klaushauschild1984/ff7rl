61990873673e8539447780f7bf4fa222e1ee5027
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2015-08-30 20:54:43 +0200

    fill context with some information

---
+++ src/main/java/de/hauschild/ff7rl/Actor.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (C) Klaus Hauschild - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Klaus Hauschild <klaus.hauschild.1984@gmail,com>, 2015
+ */
+package de.hauschild.ff7rl;
+
+/**
+ * @author Klaus Hauschild
+ */
+public enum Actor {
+
+  CLOUD,
+
+  TIFA,
+
+  BARRET,
+
+  AERIS,
+
+  RED_XIII,
+
+  CAIT_SITH,
+
+  CID,
+
+  YUFIE,
+
+  VINCENT,
+
+  SEPHIROTH,
+
+}
