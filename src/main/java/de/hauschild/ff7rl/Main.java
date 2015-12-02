2d47ffe63632217edabd2c16c5306b9f1b6dc0ab
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2015-12-02 23:52:15 +0100

    fix file header

--- src/main/java/de/hauschild/ff7rl/Main.java
+++ src/main/java/de/hauschild/ff7rl/Main.java
@@ -2,7 +2,7 @@
  * Copyright (C) Klaus Hauschild - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Klaus Hauschild <klaus.hauschild.1984@gmail,com>, 2015
+ * Written by Klaus Hauschild <klaus.hauschild.1984@gmail.com>, 2015
  */
 package de.hauschild.ff7rl;
 
